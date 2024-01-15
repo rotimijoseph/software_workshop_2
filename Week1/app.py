5732c4f4a2f180b5fdf861cb5dc02659264ec7d9
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2024-01-15 16:15:18 +0000

    add current time on page, tester version

--- Week1/app.py
+++ Week1/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from datetime import datetime 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, you've finally got this stupid thing working, congratulations!"
+
+@app.route("/time") # need to show the date too 
+def timestamp():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return f"Current Time = {current_time}"
 
 if __name__ == "__main__":
     app.run(debug=True)
