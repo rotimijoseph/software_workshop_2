e9e5b675c1a44623924f24620a0925ce3d0e9260
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2024-01-16 13:58:56 +0000

    add changes to app and request objects using dict instead of dir

--- Project4/app.py
+++ Project4/app.py
@@ -1,12 +1,15 @@
-from flask import Flask 
+from flask import Flask, Request
+
 
 app = Flask(__name__)
 
 # Write an app that shows values from the App object on one page and the Request object on another
 @app.route("/app")
 def app_obj():
-    return dir(app)
+    return str(app.__dict__)
+    # return dir(app)
 
 @app.route("/request")
 def req_obj():
-    return 
+    return str(Request.__dict__)
+    # return dir(Request)
