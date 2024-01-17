8aab9b92edd66310d63faf80c5eaa65e141aeb7d
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2024-01-17 13:36:36 +0000

    add changes to project 5, render templates and the form submission on prime factors

--- Project5/app.py
+++ Project5/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request
+from flask import Flask, Request, render_template, request
 from datetime import datetime, date
 import random
 
@@ -8,46 +8,62 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "<h1> Welcome to this really simple app.</h1>"
+    return render_template("home.html")
 
 @app.route("/timestamp")
 def timestamp():
     now = datetime.now() # datetime for the time
     today = date.today() # date for the date
+    current_time = now.strftime("%H:%M:%S")
 
-    current_time = now.strftime("%H:%M:%S") 
-
-    return f"Today is {today} and the current time is {current_time}"
+    return render_template("timestamp.html", current_time=current_time, today=today, time_stamp="Timestamp")
 
 @app.route("/quotes")
 def quotes():
     f = open('quotes.txt', 'r')
     read = f.readlines()
-    quote = random.choice(read)
-
+    random_quote = random.choice(read)
     f.close()
-    return quote
 
-@app.route("/<int:num>")
-    
-def find_prime_factors(num):
+    return render_template('quotes.html', quote_title="Quote of the Day", content=random_quote)
+
+# @app.route("/<int:num>")
+# def find_prime_factors(num):
+#     factors = []
+#     divisor = 2
+
+#     while divisor <= num:
+#         if num % divisor == 0:
+#             factors.append(divisor)
+#             num = num / divisor
+#         else:
+#             divisor += 1
+
+#     all_factors = " ".join(str(num) for num in factors)
+#     return f"The factors are: {all_factors}"
+
+@app.route('/find_prime_factors', methods=['GET', 'POST'])
+def find_prime_factors():
     factors = []
-    divisor = 2
 
-    while divisor <= num:
-        if num % divisor == 0:
-            factors.append(divisor)
-            num = num / divisor
-        else:
-            divisor += 1
+    if request.method == 'POST':
+        num = int(request.form['number'])
+        divisor = 2
+
+        while divisor <= num:
+            if num % divisor == 0:
+                factors.append(divisor)
+                num = num / divisor
+            else:
+                divisor += 1
 
     all_factors = " ".join(str(num) for num in factors)
-    return f"The factors are: {all_factors}"
+    return render_template('primefactors.html', factors=all_factors, prime_factors= "Prime Factors")
 
 @app.route("/app")
 def app_obj():
-    return str(app.__dict__)
+    return render_template('app.html', obj_name="App Object", info=dir(app))
 
 @app.route("/request")
 def req_obj():
-    return str(Request.__dict__)
+    return render_template('app.html', obj_name="Request Object", info=dir(Request))
