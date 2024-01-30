8a0a5af726943075fa73d88c265960e5dbc4d899
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2024-01-30 10:56:02 +0000

    add attempted changes to get flash messages, not fully functional yet

--- 03-solutions/03-01-registration/app/views.py
+++ 03-solutions/03-01-registration/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import RegistrationForm
 
@@ -10,5 +10,6 @@ def index():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        flash('Login for {form.username.data}')
         return redirect(url_for('index')) # change to like a welcome page I think 
     return render_template('forms.html', title="Register", form=form)
