575761e155a6a1af6bffe31ecea06d55fb8fceba
Rotimi Joseph <Rotimi@Rotimis-MacBook-Pro.local>
2024-02-22 15:39:22 +0000

    add new project for next few tasks, add login functionality but not linked to database

---
+++ 05-solutions/05-03-delete/app/forms.py
@@ -0,0 +1,61 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, DateField, TextAreaField, DecimalField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from datetime import datetime, timedelta
+from app.models import Student, Loan
+
+class RegistrationForm(FlaskForm): 
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired()])
+	dob = DateField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
+	phone = StringField('Phone Number', validators=[DataRequired(), Length(max=11)])
+	address = TextAreaField('Address', validators=[DataRequired(), Length(max=200)])
+	height = DecimalField('Height (cm)', validators=[DataRequired()])
+	weight = DecimalField('Weight (kgs)', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+	def validate_dob(self, dob):
+          today = datetime.now().date()
+          min_age_date = today - timedelta(days=18*365)
+          max_birth_date = today - timedelta(days=365*120)
+          if self.dob.data > min_age_date or self.dob.data < max_birth_date or self.dob.data > today:
+            return False
+          return True
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+    
+class AddStudentForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    firstname = StringField('Firstname')
+    lastname = StringField('Lastname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Add Student')
+
+    def validate_username(self, username):
+        if Student.query.filter_by(username=username.data).first():
+            raise ValidationError('This username is already taken. Please choose another')
+
+    def validate_email(self, email):
+        if Student.query.filter_by(email=email.data).first():
+            raise ValidationError('This email address is already registered. Please choose another')
+        
+class BorrowForm(FlaskForm):
+    student_id = StringField('Student ID', validators=[DataRequired()])
+    device_id = StringField('Device ID', validators=[DataRequired()])
+    submit = SubmitField('Borrow')
+    
+    def validate_loan(self, student_id): # see what happens if this is removed, message won't flash at the top? 
+        existing_loan = Loan.query.filter_by(student_id=student_id.data, returndatetime=None).first()
+        if existing_loan:
+            raise ValidationError('You already have a device on loan. Please return it before borrowing another device.')
+
+class ReturnForm(FlaskForm):
+    student_id = StringField('Student ID', validators=[DataRequired()])
+    device_id = StringField('Device ID', validators=[DataRequired()])
+    submit = SubmitField('Return Device')
+    
---
+++ 05-solutions/05-03-delete/app/views.py
@@ -0,0 +1,101 @@
+from flask import render_template, redirect, url_for, flash, request
+from app import app, db
+from app.forms import RegistrationForm, BorrowForm, AddStudentForm, ReturnForm, LoginForm
+from app.models import Student, Loan
+from datetime import datetime
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login for {form.username.data}', 'info')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+
+        if not form.validate_dob(form.dob):
+            flash('Please enter a valid date of birth.')
+            return render_template('registration.html', title="Register", form=form)
+        
+        flash(f'Registration for {form.username.data} received', 'success')
+        return redirect(url_for('index'))
+    return render_template('registration.html', title="Register", form=form)
+
+@app.route('/add_student', methods=['GET', 'POST'])
+def add_student():
+    form = AddStudentForm()
+    if form.validate_on_submit():
+        new_student = Student(username=form.username.data, firstname=form.firstname.data,
+                              lastname=form.lastname.data, email=form.email.data)
+        db.session.add(new_student)
+        try:
+            db.session.commit()
+            flash(f'New Student added: {form.username.data} received', 'success')
+            return redirect(url_for('index'))
+        except:
+            db.session.rollback()
+            if Student.query.filter_by(username=form.username.data).first():
+                form.username.errors.append('This username is already taken. Please choose another')
+            if Student.query.filter_by(email=form.email.data).first():
+                form.email.errors.append('This email address is already registered. Please choose another')
+    return render_template('add_student.html', title='Add Student', form=form)
+
+@app.route("/borrow", methods=["GET", "POST"])
+def borrow():
+    form = BorrowForm()
+    if form.validate_on_submit():
+        # check if student has already taken out a loan
+        existing_loan = Loan.query.filter_by(student_id=form.student_id.data, returndatetime=None).first()
+        if existing_loan:
+            flash('You already have a device on loan. Please return it before borrowing another device.') # flash shows at top of page
+            
+        # check if student is registered 
+        elif not Student.query.filter(Student.student_id == form.student_id.data).first():
+            form.student_id.errors.append("Student is not registered.") # with this append method it shows next to the form
+            
+        # check if item is already being loaned 
+        elif Loan.query.filter(Loan.device_id == form.device_id.data).first():
+            form.device_id.errors.append("Device is already out for loan.")
+        
+        # if everything is okay then new loan added to database 
+        else:
+            new_loan = Loan(student_id=form.student_id.data, device_id=form.device_id.data, borrowdatetime=datetime.now())   
+            db.session.add(new_loan)
+            try:
+                db.session.commit()
+                flash('You have succesfully loaned this device.', 'success') # success is the alert catergory not part of the string 
+                return redirect(url_for('index'))
+            except:
+                db.session.rollback()
+                if Loan.query.filter_by(student_id=form.student_id.data, returndatetime=None).first():
+                    form.student_id.errors.append('You already have a device on loan. Please return it before borrowing another device.', 'warning')
+    return render_template("borrow.html", form=form)
+
+@app.route("/return_loan", methods=['GET', 'POST'])
+def return_loan():
+    form = ReturnForm()
+    if form.validate_on_submit():
+        # check to see if student id has device attached to it 
+        try:
+            # query for the loan record that matches student and device id 
+            existing_loan = Loan.query.filter_by(student_id=form.student_id.data, device_id=form.device_id.data, returndatetime=None).first()
+            
+            if existing_loan:
+                existing_loan.returndatetime = datetime.now()
+                db.session.commit()
+                flash(f'Device: {form.device_id.data} successfully returned', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash(f'No matching loan record found for the provided student ID and device ID.')
+        except:
+            db.session.rollback()
+            
+    return render_template("return_loan.html", form=form)
